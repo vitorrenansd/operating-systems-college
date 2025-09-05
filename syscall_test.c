31565ed0c4dfa01bd80f865b11bb3739ad5c31d1
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-05 00:03:03 -0300

    update: basic syscall in Linux

---
+++ syscall_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+    printf("PID: %d\n", getpid());
+
+    int fd = open("/tmp/test_file", O_CREAT | O_RDWR, 0644);
+    write(fd, "Hello Syscall", 13);
+    close(fd);
+
+    getchar();
+    return 0;
+}
